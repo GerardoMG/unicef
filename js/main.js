d5ed40f64ca02e10e9796411683d163fec5476f9
Gerardo E. Mejía Gómez <gerardomejiag@gmail.com>
2016-03-28 18:06:26 -0600

    error explorer
    script que cambia el wmode de los videos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 //new WOW().init();
 
 $(document).ready(function() {
+   $("iframe").each(function(){
+      var ifr_source = $(this).attr('src');
+      var wmode = "wmode=transparent";
+      if(ifr_source.indexOf('?') != -1) $(this).attr('src',ifr_source+'&'+wmode);
+      else $(this).attr('src',ifr_source+'?'+wmode);
+   });
+});
+
+
+$(document).ready(function() {
 	
 	setTimeout(function(){
 		$('body').addClass('loaded');
